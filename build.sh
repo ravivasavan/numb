152030d1f1b171a12c08ffa8ffeb89deb2b6337b
ravivasavan <ravivasavan@gmail.com>
2026-04-17 13:12:46 +1000

    add app icon and bundle .icns from 1024 source

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ APP_DIR="$BUILD_DIR/$APP_NAME.app"
 CONTENTS="$APP_DIR/Contents"
 MACOS_DIR="$CONTENTS/MacOS"
 RESOURCES_DIR="$CONTENTS/Resources"
+ICON_SRC="Resources/AppIcon.png"
 
 rm -rf "$BUILD_DIR"
 mkdir -p "$MACOS_DIR" "$RESOURCES_DIR"
@@ -20,8 +21,33 @@ swiftc -O \
 
 cp Info.plist "$CONTENTS/Info.plist"
 
+# Build .icns from the 1024×1024 source
+ICONSET="$BUILD_DIR/AppIcon.iconset"
+rm -rf "$ICONSET"
+mkdir -p "$ICONSET"
+for spec in \
+    "16 icon_16x16.png" \
+    "32 icon_16x16@2x.png" \
+    "32 icon_32x32.png" \
+    "64 icon_32x32@2x.png" \
+    "128 icon_128x128.png" \
+    "256 icon_128x128@2x.png" \
+    "256 icon_256x256.png" \
+    "512 icon_256x256@2x.png" \
+    "512 icon_512x512.png" \
+    "1024 icon_512x512@2x.png"
+do
+    set -- $spec
+    sips -z "$1" "$1" "$ICON_SRC" --out "$ICONSET/$2" >/dev/null
+done
+iconutil -c icns "$ICONSET" -o "$RESOURCES_DIR/AppIcon.icns"
+rm -rf "$ICONSET"
+
 # Ad-hoc sign so Accessibility can consistently identify the bundle
 codesign --force --deep --sign - "$APP_DIR"
+
+# Nudge Finder/Dock to pick up the icon on rebuilds
+touch "$APP_DIR"
 
 echo ""
 echo "✓ built $APP_DIR"
